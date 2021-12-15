e2dbe9a8d4dc1688c38f80adc81afd3e1388e23d
Kristi Liu <ksliu@ucsb.edu>
2021-12-15 11:26:28 -0800

    add code to deal values at random

--- user-input/for-loop.sh
+++ user-input/for-loop.sh
@@ -1,2 +1,12 @@
 #for loop example 
 #we will be creating a program that "deals" values at random
+#!/bin/bash 
+cards=(1,2,3,4,5,6,7,8,9,10,10,10)
+
+echo "how many cards would you like?"
+read value 
+
+for i in $(seq 1 $value);
+do echo ${cards[random%${#cards[@]}]};
+
+done
